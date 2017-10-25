227f9c595a8c9316031dabc624a9ba7f0bb067ba
Reijer Copier <Reijer.Copier@kadaster.nl>
2017-10-25 10:50:25 +0200

    Improve delta test

--- test/pdok/featured_to_extracts/delta_test.clj
+++ test/pdok/featured_to_extracts/delta_test.clj
@@ -7,28 +7,22 @@
             [pdok.featured-to-extracts.mustache :as m]
             [clojure.test :refer :all]
             [pdok.featured-to-extracts.template :as template])
-  (:import [java.io ByteArrayInputStream])
-  )
+  (:import [java.io ByteArrayInputStream]))
 
-  (def test-db config/db)
-  (def schema config/extract-schema)
-  (def dataset "bgtv3")
-  (def extract-type "gml")
-
-
-  (defn clean-db []
-        (j/execute! test-db [(str "DROP TABLE  IF EXISTS " schema ".\"" dataset "_" extract-type "\" CASCADE")]))
-
-  (defn vacuum-tables []
-        (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".feature"])
-        (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".feature_stream"])
-        (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".timeline_col1"]))
-
+(def test-db config/db)
+(def extract-schema config/extract-schema)
+(def delta-schema config/delta-schema)
+(def dataset "bgtv3")
+(def extract-type "gml")
+  
+(defn clean-db []
+  (j/execute! test-db [(str "DROP TABLE  IF EXISTS " extract-schema ".\"" dataset "_" extract-type "\" CASCADE")])
+  (j/execute! test-db [(str "DROP TABLE  IF EXISTS " delta-schema ".\"" dataset "_" extract-type "\" CASCADE")]))
 
 (defn- get-or-add-extractset [tx dataset extract-type]
-  (let [table (str dataset "_" "gml")]
-    (j/execute! test-db [(str "CREATE SCHEMA IF NOT EXISTS " schema)])
-    (j/execute! test-db [(str "CREATE TABLE " schema ".\"" table "\" ("
+  (let [table (str dataset "_" extract-type)]
+    (j/execute! test-db [(str "CREATE SCHEMA IF NOT EXISTS " extract-schema)])
+    (j/execute! test-db [(str "CREATE TABLE " extract-schema ".\"" table "\" ("
                               "id BIGSERIAL PRIMARY KEY, "
                               "version UUID, "
                               "feature_type TEXT, "
@@ -38,13 +32,36 @@
                               "tiles INTEGER[], "
                               "xml TEXT NOT NULL, "
                               "created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")])
-    (j/execute! test-db [(str "CREATE INDEX \"" table "_version_idx\" ON " schema ".\"" table "\" "
+    (j/execute! test-db [(str "CREATE INDEX \"" table "_version_idx\" ON " extract-schema ".\"" table "\" "
+                              "USING btree(version)")])))
+
+(defn get-or-add-deltaset [tx dataset extract-type]
+  (let [table (str dataset "_" extract-type)]
+    (j/execute! test-db [(str "CREATE TABLE " delta-schema ".\"" table "\" ("
+                              "id BIGSERIAL PRIMARY KEY, "
+                              "version UUID, "
+                              "feature_type TEXT, "
+                              "valid_from TIMESTAMP WITHOUT TIME ZONE, "
+                              "valid_to TIMESTAMP WITHOUT TIME ZONE, "
+                              "publication TIMESTAMP WITHOUT TIME ZONE, "
+                              "tiles INTEGER[], "
+                              "xml TEXT NOT NULL, "
+                              "created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "
+                              "_delivery_id BIGINT)")])
+    (j/execute! test-db [(str "CREATE INDEX \"" table "_version_idx\" ON " delta-schema ".\"" table "\" "
                               "USING btree(version)")])))
 
 (def bgtv3-gml-pand-template (slurp (io/resource "templates/delta/bgtv3-gml-pand.mustache")))
 
 (defn run [file]
-    (clean-db)
+  (j/execute! test-db [(str "CREATE SCHEMA IF NOT EXISTS " delta-schema)])
+  (j/execute! test-db [(str "CREATE TABLE IF NOT EXISTS " delta-schema "._delivery ( "
+                            "id BIGINT PRIMARY KEY NOT NULL, "
+                            "file TEXT, "
+                            "from_date TIMESTAMP, "
+                            "to_date TIMESTAMP NOT NULL, "
+                            "created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")])
+  (clean-db)
   (let [_ (template/add-or-update-template {:dataset      "bgtv3"
                                             :extract-type "gml"
                                             :name         "pand"
@@ -54,14 +71,11 @@
       (with-bindings
         {
          #'core/*get-or-add-extractset* get-or-add-extractset
+         #'core/*get-or-add-deltaset* get-or-add-deltaset
          #'core/*add-metadata-extract-records* (constantly nil)
          #'core/*initialized-collection?* (constantly true)}
         (with-open [in (io/input-stream (.getFile (clojure.java.io/resource file)))]
-          (let [result (core/update-extracts dataset '("gml") {:gml {:featureRootTag "myRootTag"}} in false)]
-            (println result)
-            ))))) )
-
-
+          (let [result (core/update-extracts dataset '("gml") {:gml {:featureRootTag "myRootTag"}} in false)]))))))
 
 (comment
   (deftest new-and_deleted-test
@@ -78,13 +92,10 @@
   )
 
 (deftest delta-new-delete
-  (run "./resources/delta/bgt_pand_new_delete_G0353_cf44d351d9da42c5b6e73bfaf09f5b68.changelog")
-  )
+  (run "./resources/delta/bgt_pand_new_delete_G0353_cf44d351d9da42c5b6e73bfaf09f5b68.changelog"))
 
 (deftest delta-new-change
-  (run "./resources/delta/bgt_pand_new_change_G0213_0f868fecab1d47a0b328c6c32b767010.changelog")
-  )
-
+  (run "./resources/delta/bgt_pand_new_change_G0213_0f868fecab1d47a0b328c6c32b767010.changelog"))
 
 (def delta-gml-start-partial (slurp (io/resource "templates/delta/partials/start.mustache")))
 (def delta-gml-end-partial   (slurp (io/resource "templates/delta/partials/end.mustache")))
